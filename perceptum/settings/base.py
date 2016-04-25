58ecbcb8191de6538feb7103a7cd4eb848a7db37
mslatour <sanderlatour@gmail.com>
2016-04-25 15:58:03 +0200

    Improved dir settings.

--- perceptum/settings/base.py
+++ perceptum/settings/base.py
@@ -13,8 +13,8 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -123,9 +123,6 @@ STATICFILES_FINDERS = [
 STATICFILES_DIRS = [
     os.path.join(PROJECT_DIR, 'static'),
 ]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
--- perceptum/settings/dev.py
+++ perceptum/settings/dev.py
@@ -10,6 +10,8 @@ for template_engine in TEMPLATES:
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '1jq7oz7z&swi1e&m9&+qies7!))e%yitvz=o)72dxk9(p@p&mv'
 
+STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
