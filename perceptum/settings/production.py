5c588e3e998302cd7e3068d50c1702d199cdcfdd
mslatour <sanderlatour@gmail.com>
2016-04-25 15:11:26 +0200

    Added production files for heroku.

--- perceptum/settings/production.py
+++ perceptum/settings/production.py
@@ -1,9 +1,44 @@
 from .base import *
 
 
-DEBUG = False
+DEBUG = bool(int(os.environ.get('DEBUG_FLAG', '0')))
 
 try:
     from .local import *
 except ImportError:
     pass
+
+ADMINS = (
+    ('Sander', 'sander@perceptum.nl'),
+)
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES = {"default": dj_database_url.config()}
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+STATIC_ROOT = 'staticfiles'
+# Try to prevent collisions with other django apps that are routed.
+STATIC_URL = '/static/'
+
+if 'SECRET_KEY' in os.environ:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+if 'SITE_ID' in os.environ:
+    SITE_ID = int(os.environ.get('SITE_ID'))
+
+EMAIL_HOST_USER = os.environ['SENDGRID_USERNAME']
+EMAIL_HOST= 'smtp.sendgrid.net'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
+SERVER_EMAIL = 'website@perceptum.nl'
+
+USE_X_FORWARDED_PORT = True
--- perceptum/wsgi.py
+++ perceptum/wsgi.py
@@ -1,3 +1,5 @@
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE",
 """
 WSGI config for perceptum project.
 
@@ -14,3 +16,9 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "perceptum.settings")
 
 application = get_wsgi_application()
+	"perceptum.settings.production")
+
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
