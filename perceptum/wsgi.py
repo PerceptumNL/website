943b9a1f65d10f2546d6df7eb3b79efa0361885b
mslatour <sanderlatour@gmail.com>
2016-04-25 15:27:13 +0200

    Fixed copy-paste error.

--- perceptum/wsgi.py
+++ perceptum/wsgi.py
@@ -1,11 +1,5 @@
 import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "perceptum.settings")
-
-application = get_wsgi_application()
-	"perceptum.settings.production")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "perceptum.settings.production")
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
